c0f85cd53581cc801a5a555c6a068962c4d21f51
Songbird14 <leah.hanen.5571@gmail.com>
2025-01-22 08:37:42 -0800

    turning fix attempt

--- picarx/picarx_improved.py
+++ picarx/picarx_improved.py
@@ -222,7 +222,7 @@ class Picarx(object):
             power_scale = 1
             if (current_angle / abs_current_angle) > 0:
                 self.set_motor_speed(1, -1*speed_ak_left)
-                self.set_motor_speed(2, speed_ak_right * power_scale)
+                self.set_motor_speed(2, 1*speed_ak_right * power_scale)
             else:
                 self.set_motor_speed(1, -1*speed_ak_left * power_scale)
                 self.set_motor_speed(2, speed_ak_right )
@@ -249,11 +249,11 @@ class Picarx(object):
             power_scale = 1
             if (current_angle / abs_current_angle) > 0:   #CCW
                 logging.debug(f'current_angle',current_angle)
-                self.set_motor_speed(1, speed_ak_left * power_scale)
-                self.set_motor_speed(2, -1*speed_ak_right) 
+                self.set_motor_speed(1, -speed_ak_left * power_scale)
+                self.set_motor_speed(2, 1*speed_ak_right) 
             else:
-                self.set_motor_speed(1, speed_ak_left)
-                self.set_motor_speed(2, -1*speed_ak_right * power_scale)
+                self.set_motor_speed(1, -speed_ak_left)
+                self.set_motor_speed(2, 1*speed_ak_right * power_scale)
         else:
             self.set_motor_speed(1, speed)
             print(speed)
